da07fb5887e0e502231908c522324b0abb0bfc5b
Jubayer Juhan <davidjuhan23@gmail.com>
2023-05-15 01:55:50 +0600

    Product icon changed in sidebar

--- src/constants/sidebar/sidebarLinks.ts
+++ src/constants/sidebar/sidebarLinks.ts
@@ -1,11 +1,12 @@
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 import GroupIcon from "@mui/icons-material/Group";
+import ChairIcon from "@mui/icons-material/Chair";
 import { SidebarLinkType } from "./types";
 
 const sidebarLinks: SidebarLinkType[] = [
   { name: "Dashboard", icon: DashboardIcon, link: "/" },
-  { name: "All Products", icon: DashboardIcon, link: "/products" },
+  { name: "All Products", icon: ChairIcon, link: "/products" },
   { name: "Branches", icon: StorefrontIcon, link: "/branches" },
   { name: "Users", icon: GroupIcon, link: "/users" },
 ];
