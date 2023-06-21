d830cf7d6bf39e56d379afba3a9c762fe83bf6d9
Jubayer Juhan <davidjuhan23@gmail.com>
2023-06-21 13:14:37 +0600

    Added Custom Order Reducer

---
+++ src/redux/reducers/customOrderReducer.ts
@@ -0,0 +1,41 @@
+import {
+  ADD_CUSTOM_ORDER_ERROR,
+  ADD_CUSTOM_ORDER_PENDING,
+  ADD_CUSTOM_ORDER_SUCCESS,
+  CUSTOM_ORDER_CLEAR_MESSAGE,
+} from "../../constants/reduxActionsNames/customOrder";
+import { ReduxAction } from "../redux";
+
+const customOrderReducer = (state = {}, action: ReduxAction) => {
+  switch (action.type) {
+    case ADD_CUSTOM_ORDER_PENDING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ADD_CUSTOM_ORDER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        message: action.payload,
+      };
+    case ADD_CUSTOM_ORDER_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    case CUSTOM_ORDER_CLEAR_MESSAGE:
+      return {
+        ...state,
+        error: null,
+        message: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default customOrderReducer;
