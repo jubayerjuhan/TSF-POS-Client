852148bf0cb326fc871928974c75a793a04bc5b8
Jubayer Juhan <davidjuhan23@gmail.com>
2023-06-22 11:18:06 +0600

    added new order order status selector

--- src/pages/OrderInformation/OrderInformation.tsx
+++ src/pages/OrderInformation/OrderInformation.tsx
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSingleOrder } from "../../redux/actions/customOrder/customOrderAction";
 import { StateType } from "../../redux/redux";
+import { Form } from "react-bootstrap";
+import Button from "../../components/core/Button/Button";
 
 const OrderInformation = () => {
   const { order } = useSelector((state: StateType) => state.customOrder);
@@ -19,6 +21,16 @@ const OrderInformation = () => {
   return (
     <Pagewrapper>
       {order && <OrderInformationContent order={order} />}
+      <div className="orderStatus">
+        <h5>Change Order Status</h5>
+        <Form.Select className="mt-2" aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+        <Button title="Change Status" />
+      </div>
     </Pagewrapper>
   );
 };
