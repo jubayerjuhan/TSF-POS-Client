eefb4c16610344feb510dbbc327a71e961f131af
Jubayer Juhan <davidjuhan23@gmail.com>
2023-06-22 11:55:17 +0600

    Changed Order Status Can Be Accessed From Order Info Page

--- src/components/sections/CustomOrder/OrderStatusSelector/OrderStatusSelector.tsx
+++ src/components/sections/CustomOrder/OrderStatusSelector/OrderStatusSelector.tsx
@@ -1,14 +1,24 @@
 import { Form } from "react-bootstrap";
-import { CustomOrderFromServer } from "../../../../types/CustomOrder/CustomOrderTypes";
 import Button from "../../../core/Button/Button";
+import { OrderStatusSelectorType } from "./types";
+import { ChangeEvent } from "react";
 
-const OrderStatusSelector = ({ order }: { order: CustomOrderFromServer }) => {
+const OrderStatusSelector = ({
+  order,
+  setOrderStatus,
+}: OrderStatusSelectorType) => {
   const orderStatuses = ["Order Taken", "Shipped", "Delivered"];
 
   return (
     <div className="orderStatus">
       <h5>Change Order Status</h5>
-      <Form.Select className="mt-2" aria-label="Default select example">
+      <Form.Select
+        className="mt-2"
+        aria-label="Default select example"
+        onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+          setOrderStatus(e.target.value)
+        }
+      >
         {orderStatuses.map((orderStatus, index) => {
           return (
             <option
--- src/pages/OrderInformation/OrderInformation.tsx
+++ src/pages/OrderInformation/OrderInformation.tsx
@@ -2,13 +2,14 @@ import { useParams } from "react-router-dom";
 import Pagewrapper from "../../components/Pagewrapper/Pagewrapper";
 import OrderInformationContent from "../../components/sections/CustomOrder/OrderInformationContent/OrderInformationContent";
 import "./orderInfo.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSingleOrder } from "../../redux/actions/customOrder/customOrderAction";
 import { StateType } from "../../redux/redux";
 import OrderStatusSelector from "../../components/sections/CustomOrder/OrderStatusSelector/OrderStatusSelector";
 
 const OrderInformation = () => {
+  const [orderStatus, setOrderStatus] = useState("");
   const { order } = useSelector((state: StateType) => state.customOrder);
   const { id: orderId } = useParams();
   const dispatch = useDispatch();
@@ -20,7 +21,7 @@ const OrderInformation = () => {
   return (
     <Pagewrapper>
       {order && <OrderInformationContent order={order} />}
-      <OrderStatusSelector order={order} />
+      <OrderStatusSelector order={order} setOrderStatus={setOrderStatus} />
     </Pagewrapper>
   );
 };
